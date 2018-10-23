4aabd8d4640dda1b629322cef27269fcf5976548
Gaurav Gulati <ggulati3@gmail.com>
2018-10-23 14:28:40 -0700

    Fixing bugs in hw assignment

--- hmw_3_3/hmw_3_3/main.cpp
+++ hmw_3_3/hmw_3_3/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool checkFile(const char *s){
+    fstream check(s);
+    return (bool)check;
+}
+
+vector<string> reverseList(fstream& ifs, const char* fileName){
+    vector<string> reverseList;
+    
+    ifs.seekg(0, ifstream::beg);
+    string line;
+    while(getline(ifs, line)){
+        istringstream iss(line);
+        cout << line; 
+    }
+              
+    return reverseList;
+    
+}
+
+int main() {
+    string input;
+    cout << "Enter a file name: " ;
+    cin >> input;
+    const char * fileName = input.c_str();
+    bool check = checkFile(fileName);
+    while(check){
+        fstream ifs;
+        ofstream ofs;
+        ifs.open(fileName, fstream::in);
+        
+        reverseList(ifs, fileName);
+        
+        ifs.close();
+        ofs.close();
+        cout << "Enter a file name: " ;
+        cin >> input;
+        check = checkFile(input.c_str());
+    }
+    
+    
     return 0;
 }
