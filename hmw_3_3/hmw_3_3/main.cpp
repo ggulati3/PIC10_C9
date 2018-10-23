d2fcea5a8e31cac4a932c74fcda2a293e3d06b21
Gaurav Gulati <ggulati3@gmail.com>
2018-10-23 11:12:34 -0700

    Enviornment setup for hmw_3_3

---
+++ hmw_3_3/hmw_3_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hmw_3_3
+//
+//  Created by Gaurav Gulati on 10/23/18.
+//  Copyright © 2018 Gaurav Gulati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
