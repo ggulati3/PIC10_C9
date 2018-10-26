60aea5a1526669c7edadb792c2c091344082d019
Gaurav Gulati <ggulati3@gmail.com>
2018-10-25 19:55:00 -0700

    hmw_3_3 complete

--- hmw_3_3/hmw_3_3/main.cpp
+++ hmw_3_3/hmw_3_3/main.cpp
@@ -1,69 +1,68 @@
-//
+////
 //  main.cpp
 //  hmw_3_3
 //
 //  Created by Gaurav Gulati on 10/23/18.
-//  Copyright © 2018 Gaurav Gulati. All rights reserved.
+//  Copyright � 2018 Gaurav Gulati. All rights reserved.
 //
 
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
-#include <sstream>
-
 using namespace std;
 
-bool checkFile(const char *s){
-    fstream check(s);
-    return (bool)check;
-}
 
-string reverseLine(string line){
+string reverseLine(string line) {
     string str = "";
-    for(long i = line.length()-1; i >= 0; i--){
+    for (long i = line.length() - 1; i >= 0; i--) {
         str += line[i];
     }
     return str;
 }
 
+
 int main() {
     string input;
     vector<string> lines;
-    cout << "Enter the name of the file: " ;
+    cout << "Enter the name of the file: ";
     cin >> input;
-    cout << endl;
     
+    ifstream ifs;
     const char * fileName = input.c_str();
-    bool check = checkFile(fileName);
-    if(!check){
-        cerr << "Error: " << strerror(errno);
-    }
-    fstream ifs;
     ifs.open(fileName, fstream::in);
-    string line;
-    cout << "Original file " << input << " content:" << endl;
-    while(getline(ifs,line)){
-        cout << line << endl;
-        lines.push_back(line);
+    ifs.seekg(0, ifstream::end);
+    if (ifs.fail() || !ifs.tellg()) {
+        cout << "Error opening file " <<  input << "!" << endl;
+        return 0;
     }
-    cout << endl;
-    ifs.close();
+    else {
+        ifs.seekg(0, ifstream::beg);
+        cout << endl;
+        string line;
+        cout << "Original file " << input << " content:" << endl;
+        while (getline(ifs, line)) {
+            cout << line << endl;
+            lines.push_back(line);
+        }
+        cout << endl;
+        ifs.close();
+        
+        ofstream ofs;
+        ofs.open(fileName, ofstream::out | ofstream::trunc);
+        cout << "Reversed file " << input << " content:" << endl;
+        for (int i = 0; i < lines.size(); i++) {
+            lines[i] = reverseLine(lines[i]);
+            cout << lines[i] << endl;
+            ofs << lines[i] << endl;
+        }
+        
+        
+        ofs.close();
+    }
     
-    ofstream ofs;
-    ofs.open(fileName, ofstream::out | ofstream::trunc);
-    cout << "Reversed files " << input << " content:" << endl;
-    for(int i = 0; i < lines.size(); i++){
-        lines[i] = reverseLine(lines[i]);
-        cout << lines[i] << endl;
-        ofs << lines[i] << endl;
-    }
-
-
-    ofs.close();
-
-
     
     
     return 0;
 }
+
